3ac04396d3420974bfade80cdd4db419d3ac0ccf
Anton Egorov <anton.egoroff@gmail.com>
2018-03-22 01:01:43 +0200

    dont allow controller to move tokens unconditionally

--- contracts/MiniMeToken.sol
+++ contracts/MiniMeToken.sol
@@ -123,17 +123,10 @@ contract MiniMeToken is MiniMeTokenI {
   /// @param _amount The amount of tokens to be transferred
   /// @return True if the transfer was successful
   function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
+    // The standard ERC 20 transferFrom functionality
+    require(allowed[_from][msg.sender] >= _amount);
+    allowed[_from][msg.sender] -= _amount;
 
-    // The controller of this contract can move tokens around at will,
-    //  this is important to recognize! Confirm that you trust the
-    //  controller of this contract, which in most situations should be
-    //  another open source smart contract or 0x0
-    if (msg.sender != controller) {
-
-      // The standard ERC 20 transferFrom functionality
-      require(allowed[_from][msg.sender] >= _amount);
-      allowed[_from][msg.sender] -= _amount;
-    }
     return doTransfer(_from, _to, _amount);
   }
 
