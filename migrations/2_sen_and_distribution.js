764c4658bcc58abb74ea05fa706003347a803fbc
Anton Egorov <anton.egoroff@gmail.com>
2018-03-27 16:43:23 +0300

    fix values for a migration script to be in Wei

--- migrations/2_sen_and_distribution.js
+++ migrations/2_sen_and_distribution.js
@@ -5,13 +5,15 @@ const reserveWallet = process.env['SEN_RESERVE']
 const totalSupplyCap = process.env['SEN_TOTAL_SUPPLY'] || 2e11
 const totalReserve = process.env['SEN_TOTAL_RESERVE'] || 2e9
 
+const toWei = value => value * 1e18
+
 module.exports = async function(deployer) {
   const sen = await SEN.new()
   const distribution = await Distribution.new(
     sen.address,
     reserveWallet,
-    totalSupplyCap,
-    totalReserve
+    toWei(totalSupplyCap),
+    toWei(totalReserve)
   )
   await sen.changeController(distribution.address)
 }
