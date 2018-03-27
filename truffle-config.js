98ef92c58b6a1106ddc70bc8d9724f57f3cd5e21
Anton Egorov <anton.egoroff@gmail.com>
2018-03-27 16:21:43 +0300

    deploy to ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,17 @@
+const WalletProvider = require('truffle-wallet-provider')
+
+const infuraToken = process.env['INFURA_TOKEN']
+
+// Read and unlock keystore
+const keystore = require('fs')
+  .readFileSync('./keystore')
+  .toString()
+const pass = require('fs')
+  .readFileSync('./pass')
+  .toString()
+  .trim()
+const wallet = require('ethereumjs-wallet').fromV3(keystore, pass, true)
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -5,7 +19,12 @@ module.exports = {
     ganache: {
       host: '127.0.0.1',
       port: 7545,
-      network_id: '5777'
+      network_id: 5777
+    },
+    ropsten: {
+      provider: new WalletProvider(wallet, 'https://ropsten.infura.io/'),
+      network_id: 3,
+      gas: 4000000
     }
   }
 }
